54df9c274ebbd937edb3b4c4ad4c8d798047deab
SP0-0k <pixl3mast3r@gmail.com>
2025-02-07 11:04:40 -0700

    Got my login jsx working

--- startupReact/src/login/login.jsx
+++ startupReact/src/login/login.jsx
@@ -3,7 +3,35 @@ import React from 'react';
 export function Login() {
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+        <h1>Welcome to your auto dice roller</h1>
+            <h3>Log in and join a room</h3>
+
+
+        <form method="get" action="play.html">
+            <div className="form-group">
+            <input type="email" className="form-control centeredText verticalSpacing" id="exampleInputEmail" placeholder="your@email.com" />
+            </div>
+            <div className="form-group">
+            <input type="password" className="form-control centeredText verticalSpacing" id="exampleInputPassword" placeholder="password" />
+            </div>
+            <div className="form-group">
+            <input type="text" className="form-control centeredText verticalSpacing" id="exampleInputRoomcode" placeholder="room code" />
+            </div> 
+            <div className="centeredText verticalSpacing">
+            <button type="submit" className="btn btn-primary" disabled>Login</button>
+            <button type="button" className="btn btn-primary" disabled>Create Account</button>
+            </div>
+        </form>
+
+        <audio autoplay loop>
+            <source src="chillbackground.mp3" type="audio/ogg">
+            </source>
+        </audio>
+            
+        <br />
+        <h3>Need an excuse to leave work and play some D&D?</h3>
+        <button type="button" className="btn btn-secondary" onclick="alert('You have a meeting with the CEO')">Generate Excuse</button>
+
     </main>
   );
 }
