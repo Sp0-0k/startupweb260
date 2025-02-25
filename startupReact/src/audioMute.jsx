3231ab2f79d69ec078b7b7a49d1dc25536a100fb
SP0-0k <pixl3mast3r@gmail.com>
2025-02-25 08:32:54 -0700

    Got my mute button working correctly

--- startupReact/src/audioMute.jsx
+++ startupReact/src/audioMute.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export function AudioMuteButton(){
     const[muteState, setMuteState] = React.useState(false);
+    const[playState, setPlayState] = React.useState("Mute");
 
     React.useEffect(() => {
         setMuteState(false);
@@ -13,18 +14,22 @@ export function AudioMuteButton(){
     function toggleMuted() {
         if(muteState){
             setMuteState(false);
+            document.getElementById('sound').muted = false;
+            setPlayState("Mute")
         }
         else{
             setMuteState(true);
+            document.getElementById('sound').muted = true;
+            setPlayState("Unmute")
         }
     }
 
 
     return (
         <div>
-            <button className="btn btn-primary" onClick={toggleMuted} muted={muteState}>Mute|Unmute</button>
+            <button className="btn btn-primary" onClick={toggleMuted} muted={muteState}>{playState}</button>
             <audio id="sound" autoPlay="autoplay" loop="loop">
-                <source src="chillbackground.mp3" type="audio/ogg"/>
+                <source src="chillbackground.mp3" type="audio/ogg" mute={muteState}/>
             </audio>
         </div>
       );
