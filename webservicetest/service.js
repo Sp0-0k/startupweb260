8bea3b06db22f4ad0093952bddf39589b92259d1
SP0-0k <pixl3mast3r@gmail.com>
2025-02-27 10:38:25 -0700

    More experimenting with basic webservers

--- webservicetest/service.js
+++ webservicetest/service.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 //Creates a new object of type express named app
 
+//Express goes in order of declaration
+//In this case checking public, then /j then /r
 
 //whenever anything is requested from the server
 //the server will respond with an 404 error code
@@ -10,7 +12,45 @@ const app = express();
 //     res.send(404, '<h1>Not Found</h1>');
 // });
 
-app.use(express.static('public'));
+//Sends a file in the public directory
+// app.use(express.static('public'));
+
+
+// //Sends some json
+// //Also responds with the error code 401
+// //Code only seen in curl not in browser
+// app.get('/j', (req, res) => {
+//     res.send(401, {x: '3', y: '4'});
+// })
+
+// //Redirects to a different page
+// app.get('/r', (req, res) => {
+//     res.redirect('https://5edice.com');
+// });
+
+
+// function noBobs(req, res, next) {
+//     /bob/.test(req.path) ? res.status(401).send('No Bobs!') : next();
+// }
+
+// function isDave(req, res, next) {
+//     res.isDave = /dave/.test(req.path);
+//     next();
+// }
+      
+
+//broken check for if path has dave in it
+// app.get('*', (req, res) => {
+//     res.send('<p> ${res.isDave() ? 'yes' : 'no'} Hello</p>');
+// });
+
+
+app.use(express.json());
+
+app.put('/data', (req, res) => {
+ res.send(req.body.msg);
+});
+
 
 app.listen(3000, () =>{
     console.log('Server is running on http//localhost:3000');
